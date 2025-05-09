2332c3f76aa53f0bbba1e835a3bc0ef98068d7f6
Aidan Patt <aapatt@bu.edu>
2025-05-09 12:39:35 -0400

    Finisehd the project

--- Island_web_app/README.md
+++ Island_web_app/README.md
@@ -1,11 +1,13 @@
 # MA415-FinalProject
 Final project for MA 415, containing the code and resources used to make a shinyapp info page for North Island, New Zealand
 
-DataClean_CI.R - Contains the code where Data cleaning and data visualization
+island_dc.R - Contains the code where Data cleaning and data visualization
   intially took place
 
-island_data - contains folders for various csv e.g data on smoking, religion, etc
+island_data - contains folders for various csv files e.g data on smoking, religion, etc
 
 My_functions.R - functions made or given from my professor used throughout the project
 
 app.r - is the code for shinyapp webpage
+
+www - the folder for pictures
--- Island_web_app/app.R
+++ Island_web_app/app.R
@@ -1,5 +1,11 @@
 # MA 415 FINAL PROJECT WEB APP
 
+
+############## MUST SET DIRECTORY TO Island_web_app FOLDER OPENED UP SO THAT
+### THE FUNCTION THAT READS THE FOLDER (island_data) IN Island_web_app 
+###### CAN PROPERLY DO SO ###########
+
+
 # Find out more about building applications with Shiny here:
 #    https://shiny.posit.co/
 
@@ -81,6 +87,8 @@ ui <- page_fluid(
               
               p("New Zealand as a whole is ranked 7th in the world for its education system. School is mandatory from ages 6 through 16, and public schools are well funded. The majority of classes are taught in English however there are public schools where te reo Māori is used to teach and the curriculum has more of a foundation on Māori culture. There is outdoor education, usually in Yeaers 5,6,7,8,9, and 10 the students will spend a few nights camping outdoors with their peers building resourcefulness and other useful skills.",
                 style = "margin-top: 20px; margin-bottom: 30px"),
+              p("The data shows that throughout the various regions in North Island there is an overall trend of the percentage of the population in increasing for those obtaining a Bachelors degree equivalent or higher. For example in Auckland, one of the most populated cities in New Zealand, in 2006 the percentage who had a bachelors or Masters was 22.6%, but in 2018 the combined percentage was 28.4%. This slight trend is seen all over, and supports the fact that New Zealand invests resources into its education of its citizens.",
+                style = "margin-bottom: 30px"),
               p("Below are ways of selecting various regions in the North Island to examine the education levels over time. The table allows you to look at the values for one year at a time, while the plot shows how things have changed over time.",
                 style = "margin-bottom:30px"),
               
@@ -129,7 +137,8 @@ ui <- page_fluid(
                          
                          h2("Examining the Various Ethnic Backgrounds",
                             style = "text-align: left; font-style: bold; font-size: 40px"),
-                        
+                        p("European is the most common ethnic group in North Island, which according to the history of colonization and other factors makes sense. However across the three main cities, of Auckland, Wellington, and Hamilton there appears to be a slight decrease in European percentage and an increase in other various ethnicities, with Auckland having an almost 10% jump in the percentage of the population that is Asian from 2006 to 2018.",
+                          style = "margin-top:30px; margin-bottom:20px"),
                          p("Below are ways of selecting various regions in the North Island to examine the prevalence of ethnic groups over time. The table allows you to look at the specific values for a specific year, while the plot shows how things have changed over time.",
                            style = "margin-top: 30px; margin-bottom:30px"),
                          
@@ -153,8 +162,9 @@ ui <- page_fluid(
                 tabPanel("Expatriate Data",
                          h2("How Many Expatriates Are in North Island?",
                             style = "text-align: left; font-style: bold; font-size: 40px"),
-                         
-                         p("Below are ways of selecting various regions in the North Island to examine expatriate information to examine the number of people born overseas and how long they have been in New Zealand . The table allows you to look at the specific values for a specific year, while the plot shows how things have changed over time.",
+                         p("Across North Island, over 30% of the population that lives there currently that was born overseas has been there for less than ten years. North Island, New Zealand seems to be an attractive place to live that is causing an increase in the people moving there. For example in Auckland in 2006 the number of people born overseas that had just recently moved to Auckland was 28,299, but in 2018 the number was 35,046. Considering it is one of the safest places for expatriates and is consistently rated highly for its quality of life this increase makes sense.",
+                           style = "margin-top:30px; margin-bottom:20px"),
+                         p("Below are ways of selecting various regions in the North Island to examine expatriate information to examine the number of people born overseas and how long they have been in New Zealand . The table allows you to look at the specific values for a specific year, and the plot shows the distribution for that year.",
                            style = "margin-top: 30px; margin-bottom:30px"),
                          fluidRow(column(8,
                                          selectInput("exp_place", "Places",
@@ -177,8 +187,9 @@ ui <- page_fluid(
                 tabPanel("Maori Data",
                          h2("The Indigenous People of North Island",
                             style = "text-align: left; font-style: bold; font-size: 40px"),
-                         
-                         p("Below are ways of selecting various regions in the North Island to compare the Maori population with the Non-Maori population. The table allows you to look at the specific values for a specific year, while the plot shows how things have changed over time.",
+                         p("While this relationship is somewhat visible by looking at the ethnicity tab, this data highlights the small portion of the Māori population compared to the Non-Māori populaiton across various regions of North Island, New Zealand. The gap appears to be growing over time which aligns with the fact that there are more people moving to North Island, New Zealand.",
+                           style = "margin-top:30px;margin-bottom:20px"),
+                         p("Below are ways of selecting various regions in the North Island to compare the Māori population with the Non-Māori population. The table allows you to look at the specific values for a specific year, while the plot shows how things have changed over time.",
                            style = "margin-top: 30px; margin-bottom:30px"),
                          
                          
@@ -204,6 +215,10 @@ ui <- page_fluid(
                          h2('What are the predominant religions and beliefs?',
                             style = "text-align: left; font-style: bold; font-size: 40px"),
                          
+                         p("The predominant religion appears to be Christianity, however the common trend across North Island appears to be a decrease in Christian beleifs and an increase in Atheism. Depending on the region there might be a higher percentage of people who beleive in the Māori religions, however there is also a consistent decrease in these percentages over time.",
+                           style="margin-top: 20px; margin-bottom:20px"),
+                         p("Below are ways of selecting different regions across North Island to examine the prevalence of different religious groups. The table allows you to look at specific values for a given year and region, while the plot shows how values have changed over time for a selected region.",
+                           style = "margin-top: 30px; margin-bottom:30px"),
                          fluidRow(column(8,
                                          selectInput("rel_place", "Places",
                                                      choices = religion$`Area_description`)
@@ -250,7 +265,10 @@ ui <- page_fluid(
                 accordion_panel(
                   title = "Examining Housing Data",
                   icon = bsicons::bs_icon("menu-app"),
-                  "Examining Housing Data",
+                  p("The amount of households that are owned or partly owned have been steadily increasing.",
+                    style = "margin-bottom:20px"),
+                  p("Below is a way to examine the property tenure information for homes across North Island. The plot shows how property holdings have changed over time while the table shows values for specific years",
+                    style = "margin-bottom:30px"),
 
                   fluidRow(column(8,
                                   selectInput("house_place", "Places",
@@ -272,7 +290,11 @@ ui <- page_fluid(
                 accordion_panel(
                   title = "Incomes",
                   icon = bsicons::bs_icon("bar-chart"),
-                  "How much are people making?",
+                  
+                  p("There appears to be a bimodal wealth distribution pointing to signs of possible wealth inequality.",
+                    style = "maring-bottom:20px"),
+                  p("Below is a way to examine the income distribution for a specific region during 2018. The table provides the specific values shown in the graph.",
+                    style="margin-bottom:30px"),
 
                   fluidRow(column(8,
                                   selectInput("inc_place", "Places",
@@ -290,8 +312,11 @@ ui <- page_fluid(
                 accordion_panel(
                   title = "Employment",
                   icon = bsicons::bs_icon("calendar-date"),
-                  "What is the work life",
-
+                  p("Across most regions the most significant increase has been the number of people employed full time which is a good sign for the economic quality of life.",
+                    ),
+                  p("Below are ways to examine the prevalence of types of employment across various regions. The table gives specific values for a given year, while the graph shows how values have changed over time.",
+                    style = "margin-bottom:30px"),
+                  
                   fluidRow(column(8,
                                   selectInput("job_place", "Places",
                                               choices = job$`Area_description`)
@@ -320,7 +345,10 @@ ui <- page_fluid(
               h2("How prevalent is smoking?",
                  style = "text-align: left; font-style: bold; font-size: 40px; margin-top: 20px; margin-bottom:40px"
               ),
-              
+              p("Across various regions in North Island smoking has decreased, and there has been an increase in the percentage of the population who has never smoked or is an ex smoker.",
+                style = "margin-top:30px; margin-bottom:20px"),
+              p("Below are ways to adjust the region and year to examine the prevalence of smoking behaviors in North Island. The table shows specific values for a given year, while the plot shows how these values have changed over time.",
+                style = "margin-top:20px; margin-bottom:30px"),
               fluidRow(
                 layout_columns(
                   card(
@@ -350,7 +378,10 @@ ui <- page_fluid(
     nav_panel("Transportation",
               h2("How do North Islanders commute?",
                  style = "margin-top: 10px; text-align-left; font-style: bold; font-size: 40px; margin-bottom: 25px"),
-              
+              p("The prevalence of walking and the steep terrain even in cities is considered one reason people who live in New Zealand are so healthy. However while there is a decent amount of students who are able to walk to school the prevalence depends on wether or not it is a city or rural area, with more students walking in the cities.",
+                style = "margin-top:30px; margin-bottom:20px"),
+              p("Below is a way to select a specific place or region in North Island to see the prevalence of various commuting styles for students. The table shows the specific values for 2018 that are also shown in the graph.",
+                style = "margin-top: 20px; margin-bottom:30px"),
               fluidRow(
                 layout_columns(
                   card("Region in North Island",
@@ -390,6 +421,8 @@ ui <- page_fluid(
               a("Shiny Layouts", href = "https://shiny.posit.co/r/layouts/"),
               p(),
               a("Mastering Shiny in R", href = "https://mastering-shiny.org/basic-case-study.html"),
+              p(),
+              a("MA 415 Class Resources", href = "https://learn.bu.edu/ultra/courses/_167787_1/cl/outline"),
               
               p("For North Island Information:",
                 style = "margin-top: 40px; font-style: bold; font-size: 15px, margin-bottom:20px"),
